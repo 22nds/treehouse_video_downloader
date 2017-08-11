5f93f447ccb6e41b731157d5fd32e894f374ad96
Maja Kraljic <maja.kraljic@gmail.com>
2017-08-11 15:44:13 +0200

    added subtitles and course downloading

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # Treehouse video downloader
 
 ## Summary
-Treehouse Video Downloader downloads videos from the specified [Treehouse links](http://www.teamtreehouse.com).
+Treehouse Video Downloader downloads videos (with optional subtitles) from the specified [Treehouse courses and workshops](http://www.teamtreehouse.com).
 
 ## Dependencies
+Install these dependencies:
 - [youtube-dl](https://rg3.github.io/youtube-dl/)
 - [BeautifulSoup 4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
 
 ## Usage
-To use it change the **USERNAME** and **PASSWORD** variables in `main.py` file and define
-the **links** you would like to download in the `links.txt` file. Example list of the files is available.
+Before usage you need to
+- change the **USERNAME** and **PASSWORD** variables in `main.py` file and
+- define the **courses/workshops** you would like to download in the `links.txt` file. Example list of the courses/workshops is available.
 
-If the **download of the video fails**, the link will be saved in `log.txt`.
+If you would like to download the **subtitles** of the videos set `SUBTITLES = True`. They are not downloaded by default.
+
+If the **download of the video fails**, the course URL will be saved in `log.txt`.
 
 ## Notes
 Because youtube-dl login does not work for proper authentication of the Treehouse user I have used python 'requests' module to get the correct video link.
 
 Hopefully youtube-dl will be extended to cover Treehouse soon. Here is the [issue](https://github.com/rg3/youtube-dl/issues/9836).
 
-Script tested on the youtube-dl version 2016.07.28 and python 3.4.
+Script was tested with the `youtube-dl 2017.07.30.1` and `python 3.5.3`.
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ To use it change the username and password and define
 the link you would like to download in the links.txt file.
 
 If download fails, the link will be saved in log.txt
-
 """
+
 import requests
+import os
+import re
 
 from bs4 import BeautifulSoup
 import youtube_dl
@@ -16,33 +18,152 @@ import youtube_dl
 USERNAME = 'your_username'
 PASSWORD = 'your_password'
 
+# Download subtitles of the videos
+SUBTITLES = False
+HOME_DIR = os.getcwd()
+
+
+def move_to_course_directory(title):
+    """Check if current directory is home directory. If not, change to it.
+    Make a course directory and move to it.
+    If course directory already exists, just move to it.
+    If everything fails break the program.
+    """
+
+    # Move to home directory if we are somewhere else (e.g. course subdir)
+    if os.getcwd() != HOME_DIR:
+        os.chdir(HOME_DIR)
+    try:
+        # Make a directory with course name
+        os.mkdir(title)
+        os.chdir(title)
+    except FileExistsError:
+        # Position yourself in course directory
+        os.chdir(title)
+    except:
+        print('Could not create subdirectory for the course: {}'.format(title))
+
+
+def getID(link):
+    """Go to the web page with the video and extract it's ID
+    """
+    html = requests.get(link)
+    soup = BeautifulSoup(html.text, "html.parser")
+    for id in soup.select('meta[property="og:url"]'):
+        parts = id['content'].split('/')
+        id = parts[-1]
+        return id
+
+
+def getSubtitles(id, name):
+    """ Download and rename subtitle to match the downloaded videos
+    Subtitle is located at https://teamtreehouse.com/videos/{id}}/captions
+    """
+    subtitlesLink = 'https://teamtreehouse.com/videos/{}/captions'.format(id)
+    response = requests.get(subtitlesLink)
+    if response.status_code == 200:
+        contentDisposition = response.headers['Content-Disposition']
+        parts = contentDisposition.split('"')
+        title = '{}-{}'.format(name, parts[-2])
+        content = response.text
+        with open(title, 'w') as f:
+            f.write(content)
+        return 0
+
+
+def getLinksCourse(link):
+    """ Get the content of stages and extract the links to videos
+    """
+    # Prepare URL with stages
+    linkToStages = '{}{}'.format(link, '/stages')
+    html = requests.get(linkToStages)
+    soup = BeautifulSoup(html.text, "html.parser")
+
+    # find all urls of the videos (ignore reviews and questions)
+    videos = []
+    for a in soup.select('a[href^="/library/"]'):
+        if (a.select('.video-22-icon')):  # if video icon is there
+            videoLink = '{}{}'.format('https://teamtreehouse.com', a['href'])
+            videos.append(videoLink)
+    print(videos)
+    return videos
+
+
+def getLinksWorkshop(link):
+    """ Get the links to videos from workshop page
+    """
+    html = requests.get(link)
+    soup = BeautifulSoup(html.text, "html.parser")
+
+    # find all urls of the videos
+    videos = []
+    for a in soup.select('li.workshop-video a[href^="/library/"]'):
+        vidLink = '{}{}'.format('https://teamtreehouse.com', a['href'])
+        videos.append(vidLink)
+    return videos
+
+
+def getLinkWorkshop(link):
+    """ Get the link to video from workshop page - only one video workshop
+    """
+    html = requests.get(link)
+    soup = BeautifulSoup(html.text, "html.parser")
+
+    # find all urls of the videos
+    videos = []
+    for a in soup.select('a#workshop-hero'):
+        vidLink = '{}{}'.format('https://teamtreehouse.com', a['href'])
+        videos.append(vidLink)
+    return videos
+
+
 for link in open('links.txt'):
     try:
         link = link.strip()
-        print(link)
-        html = requests.get(link, auth=(USERNAME, PASSWORD))
+        print('Downloading: {}'.format(link))
 
-        soup = BeautifulSoup(html.text, "html.parser")
+        videos = getLinksWorkshop(link) or getLinkWorkshop(link) or getLinksCourse(
+            link)
 
-        # Title
-        h1 = soup.h1.contents[0]
-        print(h1)
+        # generate folder name and move to it
+        parts = link.split('/')
+        title = parts[-1]
+        move_to_course_directory(title)
 
-        # Output with the title of the video
-        output = u'%(id)s-' + h1 + u'.%(ext)s'
+        for video in videos:
 
-        # Video source link
-        tag = soup.video
-        videolink = tag.source['src']
+            html = requests.get(video, auth=(USERNAME, PASSWORD))
+            soup = BeautifulSoup(html.text, "html.parser")
 
-        # Youtube-dl options
-        options = {
-            'outtmpl': output,
-        }
+            # Extract title for filename
+            h1 = soup.h1.contents[0]
 
-        with youtube_dl.YoutubeDL(options) as ydl:
-            ydl.download([videolink])
+            # Output with the title of the video
+            output = u'%(id)s-' + h1 + u'.%(ext)s'
+
+            # Video source link
+            tag = soup.video
+            videolink = tag.source['src']
+
+            # Youtube-dl options
+            options = {
+                'outtmpl': output,
+                'verbose': True
+            }
+
+            with youtube_dl.YoutubeDL(options) as ydl:
+
+                ydl.download([videolink])
+
+                if (SUBTITLES):
+                    ID = getID(video)
+                    info = ydl.extract_info(videolink, download=False)
+                    name = info.get('title', None)
+                    subs = getSubtitles(ID, name)
+
     except:
-        log = open('log.txt', 'w')
+        os.chdir(HOME_DIR)
+        log = open('log.txt', 'a')
         log.write(link)
+        log.write('\n')
         log.close()
